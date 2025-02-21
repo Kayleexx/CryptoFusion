670383d9455d5786e6cb118d40c679538b5dfb25
Mitali Sinha <105365766+Kayleexx@users.noreply.github.com>
2025-02-21 23:41:07 +0530

    fixed some issues

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ const App = () => (
               <Route path="/news" element={<News />} />
             </Routes>
           </div>
+  
         </Layout>
         <div className="footer">
           <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
