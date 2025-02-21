961286eeb43078189aa503112e23f01b6d82b744
Mitali Sinha <105365766+Kayleexx@users.noreply.github.com>
2025-02-21 23:41:34 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App'; 
 import store from './app/store'; 
 import { Provider } from 'react-redux';
-
 import 'antd/dist/reset.css';
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
